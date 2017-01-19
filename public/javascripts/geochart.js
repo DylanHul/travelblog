623680b133fb60017a1f914e5959de64dd040b13
DylanHul <dylan.hulain@gmail.com>
2017-01-19 16:07:12 +1300

    geochart added, not working

--- public/javascripts/geochart.js
+++ public/javascripts/geochart.js
@@ -1,1 +1,22 @@
 
+  function drawMap() {
+    var data = google.visualization.arrayToDataTable([
+      ['Country', 'Popularity'],
+      ['Germany', 200],
+      ['United States', 300],
+      ['Brazil', 400],
+      ['Canada', 500],
+      ['France', 600],
+      ['RU', 700]
+    ]);
+
+    var options = {};
+    options['dataMode'] = 'regions';
+
+    var container = document.getElementById('regions_div');
+    var geomap = new google.visualization.GeoMap(container);
+
+    geomap.draw(data, options);
+  };
+
+  module.exports = drawMap
--- routes/destinations.js
+++ routes/destinations.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var drawMap = require('../public/javascripts/geoChart.js')
 
 /* GET destination page. */
 router.get('/', function(req, res, next) {
-  res.render('destinations', { title: 'Top Ten' });
+  res.render('destinations',  { map: drawMap});
+  // console.log(geoChart);
+  // console.log(drawMap, "mapdraq");
 });
 
 module.exports = router;
